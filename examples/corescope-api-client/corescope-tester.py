7eeb237efd87603980e37789a8691ddf60e74888
Bedran Karakoc <bedran.karakoc@srs.io>
2021-12-09 14:19:23 +0100

    Updated configuration parameters for the tester script

--- examples/corescope-api-client/corescope-tester.py
+++ examples/corescope-api-client/corescope-tester.py
@@ -32,13 +32,28 @@ from corescope_api_client.api.default import get_ue_status
 from corescope_api_client.models import dto_ue_status
 
 
+######################################
+# Configure the following parameters #
+######################################
+
+corescope_ip = "127.0.0.1"
+corenetwork_ip = "10.45.0.1"
+gnb_id = 0
+
+imsi = "001010123456780"
+imei = "353490069873310"
+opc = "63bfa50ee6523365ff14c1f45f88737d"
+k = "00112233445566778899aabbccddeeff"
+
+
 class coreScopeTester:
     def __init__(self, ip, gnb_id):
         self.client = Client(base_url="http://{}:8000".format(ip), timeout=5)
         self.gnb_id = gnb_id
 
     def switch_on(self, ue_id):
-        ue_status_update_args = dto_ue_status_update.DtoUeStatusUpdate(switch_on=True)
+        ue_status_update_args = dto_ue_status_update.DtoUeStatusUpdate(
+            switch_on=True)
 
         print("Switching UE on")
         toggle_state_ue_data: Response[dto_ue_status_update.T] = update_ue_state.sync_detailed(
@@ -46,7 +61,8 @@ class coreScopeTester:
         return str(toggle_state_ue_data.status_code).startswith("2")
 
     def switch_off(self, ue_id):
-        ue_status_update_args = dto_ue_status_update.DtoUeStatusUpdate(switch_on=False)
+        ue_status_update_args = dto_ue_status_update.DtoUeStatusUpdate(
+            switch_on=False)
 
         print("Switching UE off")
         toggle_state_ue_data: Response[dto_ue_status_update.T] = update_ue_state.sync_detailed(
@@ -57,7 +73,9 @@ class coreScopeTester:
         ue_args = dto_ue_args.DtoUeArgs()
 
         # usim args
-        ue_args.usim = dto_ue_args.DtoUeUsimArgs()
+        ue_args.usim = dto_ue_args.DtoUeUsimArgs(
+            imsi=imsi, imei=imei, opc=opc, k=k
+        )
 
         # nas args
         ue_args.nas_5g = dto_ue_args.DtoUeNas5gArgs()
@@ -79,7 +97,8 @@ class coreScopeTester:
             filename="/tmp/corescope.log")
 
         print("Adding UE")
-        add_ue_data: Response[dto_ue_id.DtoUeId] = create_ue.sync_detailed(client=self.client, gnb_id=0, json_body=ue_args)
+        add_ue_data: Response[dto_ue_id.DtoUeId] = create_ue.sync_detailed(
+            client=self.client, gnb_id=0, json_body=ue_args)
 
         if str(add_ue_data.status_code).startswith("2"):
             ue_id = add_ue_data.parsed.ue_id
@@ -88,20 +107,19 @@ class coreScopeTester:
             return -1
 
     def is_connected(self, ue_id):
-        ue_status_data: Response[dto_ue_status.T] = get_ue_status.sync_detailed(client=self.client, gnb_id=0, ue_id=ue_id)
+        ue_status_data: Response[dto_ue_status.T] = get_ue_status.sync_detailed(
+            client=self.client, gnb_id=0, ue_id=ue_id)
         if not str(ue_status_data.status_code).startswith("2"):
-            print("Getting UE status failed. Exiting... (%s)" % ue_status_data.status_code)
+            print("Getting UE status failed. Exiting... (%s)" %
+                  ue_status_data.status_code)
             exit(1)
         else:
             return ue_status_data.parsed.active and ue_status_data.parsed.initiated
 
     def is_interface_up(self, interface):
-        return interface in netifaces.interfaces() 
+        return interface in netifaces.interfaces()
 
-# Configure the following arguments
-corescope_ip = "127.0.0.1"
-corenetwork_ip = "10.45.0.1"
-gnb_id = 0
+
 corescope_tester = coreScopeTester(corescope_ip, gnb_id)
 
 ###
@@ -140,7 +158,8 @@ if not corescope_tester.is_connected(ue_id):
 # send some data
 ###
 print("Sending ping over tun interface...")
-ping_proc = Popen(["ping", corenetwork_ip, "-I", "tun_ue_{}".format(ue_id), "-c", "3"])
+ping_proc = Popen(["ping", corenetwork_ip, "-I",
+                   "tun_ue_{}".format(ue_id), "-c", "3"])
 stdout, stderr = ping_proc.communicate()
 
 print("Sleeping for a while")
